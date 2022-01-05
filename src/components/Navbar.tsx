0ec856bde8704164c4da891e25c7a24c942c99e6
Romullo Queiroz de Assis Bernardo <romulloqueiroz@hotmail.com>
2022-01-06 08:49:05 +0900

    Init & Navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => (
+  <nav className='navbar navbar-dark bg-primary'>
+    <div className='container-fluid'>
+      <span className='navbar-brand mb-0 h1'>Navbar</span>
+    </div>
+  </nav>
+)
+
+export default Navbar
